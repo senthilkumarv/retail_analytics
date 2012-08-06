5ac7f4e04df72dca1581934bc3982f7fcda20f59
Avishek Sen Gupta <avishek@Bane.(none)>
2012-08-06 09:27:52 +0530

    Adding basket mix

--- simple_customer.rb
+++ simple_customer.rb
@@ -13,11 +13,15 @@ class Customer
 	end
 end
 
+def p(product_id, quantity)
+	{:product_id => product_id, :quantity => quantity}
+end
+
 customer_frequency_distribution = {1..1 => 0.4, 2..3 => 0.2, 4..5 => 0.1, 6..10 => 0.2}
-order_value_frequency_distribution = {1..500 => { :fraction_customers =>0.4, :basket_mix => []}, 
-				      501..2000 => { :fraction_customers =>0.3, :basket_mix => []}, 
-				      2001..4000 => { :fraction_customers =>0.2, :basket_mix => []}, 
-				      4001..10000 => { :fraction_customers =>0.1, :basket_mix => []}}
+order_value_frequency_distribution = {1..500 => { :fraction_customers =>0.4, :basket_mix => [p(1,4), p(4,5)]}, 
+				      501..2000 => { :fraction_customers =>0.3, :basket_mix => [p(1,4), p(4,5)]}, 
+				      2001..4000 => { :fraction_customers =>0.2, :basket_mix => [p(1,4), p(4,5)]}, 
+				      4001..10000 => { :fraction_customers =>0.1, :basket_mix => [p(1,4), p(4,5)]}}
 recency_frequency_distribution = { 0..7 => 0.3, 8..30 => 0.5, 31..80 => 0.2}
 
 
@@ -26,6 +30,7 @@ num_customers = 100
 ids = Set.new((1..num_customers).to_a)
 transactions = []
 
+transaction_number = 0
 customer_frequency_distribution.each_pair do |k,v|
 	frequency = (v * num_customers).to_i
 	frequency.times do |f|
@@ -33,7 +38,8 @@ customer_frequency_distribution.each_pair do |k,v|
 		as_array = ids.to_a
 		customer_index = as_array[rand(as_array.length).to_i]
 		num_transactions.times do |i|
-			transactions << {:id => customer_index}
+			transactions << {:id => customer_index, :transaction_id => transaction_number}
+			transaction_number += 1
 		end
 		ids.delete(customer_index)
 	end
@@ -42,13 +48,13 @@ end
 ids = Set.new((1..num_customers).to_a)
 order_value_frequency_distribution.each_pair do |k,v|
 	frequency = (v[:fraction_customers] * num_customers).to_i
-	puts frequency
 	frequency.times do |f|
 		as_array = ids.to_a
 		customer_index = as_array[rand(as_array.length).to_i]
 		selected = transactions.select {|t| t[:id] == customer_index}
 		selected.each do |t|
 			t[:value] = (rand(k.end - k.begin) + k.begin).to_i
+			t[:basket] = v[:basket_mix]
 		end
 		ids.delete(customer_index)
 	end
@@ -79,9 +85,11 @@ recency_frequency_distribution.each_pair do |k,v|
 end
 
 handle = File.open("op.csv", "w")
-handle.puts("CustomerID, OrderValue, Date")
+handle.puts("CustomerID, OrderValue, Date, TransactionID, ProductID, Quantity")
 transactions.each do |t|
-	handle.puts("#{t[:id]}, #{t[:value]}, #{t[:date]}")
+	t[:basket].each do |mix|
+		handle.puts("#{t[:id]}, #{t[:value]}, #{t[:date]}, #{t[:transaction_id]}, #{mix[:product_id]}, #{mix[:quantity]}")
+	end
 end
 
 handle.close
